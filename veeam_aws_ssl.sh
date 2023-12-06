ceaba7c52ae0b739e98e748b057ee1bc1213da0d
Scott John <29280467+swjpilot@users.noreply.github.com>
2023-12-06 13:40:02 -0500

    Production build 1.0

--- veeam_aws_ssl.sh
+++ veeam_aws_ssl.sh
@@ -9,37 +9,46 @@
 ##      .Notes
 ##      NAME:  veeam_aws_ssl.sh
 ##      ORIGINAL NAME: veeam_aws_ssl.sh
-##      LASTEDIT: 21/06/2020
-##      VERSION: 1.0
+##      PROPERTIES: veeam_aws_ssl.properties
+##      LASTEDIT: 12/06/2023
+##      VERSION: 2.0
 ##      KEYWORDS: Veeam, SSL, Let's Encrypt
-   
-##      .Link
-##      https://jorgedelacruz.es/
-##      https://jorgedelacruz.uk/
 
-# Configurations
-##
-# Endpoint URL for login action
-veeamDomain="YOURVEEAMAWSAPPLIANCEDOMAIN"
-veeamSSLPassword="YOURVEEAMSSLPASSWORD" #Introduce a password that will be use to merge the SSL into a .PFX
-veeamOutputPFXPath="/tmp/bundle.pfx"
-veeamUsername="YOURVEEAMBACKUPUSER"
-veeamPassword="YOURVEEAMBACKUPPASS"
-veeamBackupAWSServer="https://YOURVEEAMBACKUPIP"
-veeamBackupAWSPort="11005" #Default Port
+#Read Properties File with Configuration
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [[ -s $SCRIPT_DIR/veeam_aws_ssl.properties ]] 
+then
+    while read line; do
+        export $line
+    done < $SCRIPT_DIR/veeam_aws_ssl.properties
+else
+    echo "Properties File Not found in same directory as script is being executed from"
+fi
+if [[ "$ACMEecc" == "true" ]]
+then
+    ecc=$veeamDomain
+    ecc+=_ecc
+else
+    ecc=$veeamDomain
+fi
 
-veeamBearer=$(curl -X POST --header "Content-Type: application/x-www-form-urlencoded" --header "Accept: application/json" -d "Username=$veeamUsername&Password=$veeamPassword&refresh_token=&grant_type=Password&mfa_token=&mfa_code=" "$veeamBackupAWSServer:$veeamBackupAWSPort/api/v1/token" -k --silent | jq -r '.access_token')
+authResp=$(curl -X POST --header "Content-Type: application/x-www-form-urlencoded" --header "Accept: application/json" --header "x-api-version: 1.4-rev0" -d "Username=$veeamUsername&Password=$veeamPassword&grant_type=Password" "$veeamBackupAWSServer:$veeamBackupAWSPortapi/api/v1/token" -k --silent)
 
+echo $authResp
+
+veeamBearer=$(jq -r '.access_token' <<< $authResp)
+
+echo $veeamBearer
 ##
 # Veeam Backup for AWS SSL PFX Certificate Creation. This part will combine Let's Encrypt SSL files into a valid .pfx for Microsoft for AWS
 ##
-openssl pkcs12 -export -out $veeamOutputPFXPath -inkey /root/.acme.sh/$veeamDomain/$veeamDomain.key -in /root/.acme.sh/$veeamDomain/fullchain.cer -password pass:$veeamSSLPassword
+openssl pkcs12 -export -out $veeamOutputPFXPath -inkey /root/.acme.sh/$ecc/$veeamDomain.key -in /root/.acme.sh/$ecc/fullchain.cer -password pass:$veeamSSLPassword -certpbe PBE-SHA1-3DES -keypbe PBE-SHA1-3DES -nomac
 
 ##
 # Veeam Backup for AWS SSL Certificate Push. This part will retrieve last Let's Encrypt Certificate and push it
 ##
-veeamVBAURL="$veeamBackupAWSServer:$veeamBackupAWSPort/api/v1/settings/certificates/upload"
+veeamVBAURL="$veeamBackupAWSServer:$veeamBackupAWSPortapi/api/v1/settings/certificates/upload"
 
-curl -X POST "$veeamVBAURL" -H "accept: application/json" -H "x-api-version: 1.0-rev0" -H "Authorization: Bearer $veeamBearer" -H "Content-Type: multipart/form-data" -F "certificateFile=@$veeamOutputPFXPath;type=application/x-pkcs12" -F "certificatePassword=$veeamSSLPassword" -k
+curl -X POST "$veeamVBAURL" -H "accept: application/json" -H "x-api-version: 1.4-rev0" -H "Authorization: Bearer $veeamBearer" -H "Content-Type: multipart/form-data" -F "certificateFile=@$veeamOutputPFXPath;type=application/x-pkcs12" -F "certificatePassword=$veeamSSLPassword" -k
 
 echo "Your Veeam Backup for AWS SSL Certificate has been replaced with a valid Let's Encrypt one. Go to https://$veeamDomain"
